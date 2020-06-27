92f217a9aa07ee80fd7a54d5f79610815af1cd03
Dorokhovich Mikhail <mikhail@dorokhovich.ru>
2020-06-27 13:55:55 +0300

    added routes and applied prettier

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products',
+  },
+  {
+    path: 'products/pages/:id',
+  },
+  {
+    path: 'products/:id',
+  },
+  {
+    path: 'card',
+  },
+  {
+    path: 'wishlist',
+  },
+  {
+    path: 'orders',
+  },
+  {
+    path: 'orders/:id',
+  },
+  {
+    path: 'me',
+  },
+  {
+    path: 'auth',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
