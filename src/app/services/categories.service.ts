fed1faf4fc4140f5c92692bce02f413f0419a910
Dorokhovich Mikhail <mikhail@dorokhovich.ru>
2020-06-28 14:31:46 +0300

    added common categories and rollbacked cdk

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { ICategory } from '../common/category';
+import categories from './categories.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
-  public categories: ICategory[] = []
+  public categories: ICategory[] = categories;
+
   constructor() { }
 }
