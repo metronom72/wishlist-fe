7817e1a7e0ec82582f6c652582594e633b0fd887
George <collectionpiter@gmail.com>
2020-07-03 00:26:35 +0300

    stage indicators in progress

--- src/app/components/stage-indicator/stage-indicator.component.ts
+++ src/app/components/stage-indicator/stage-indicator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-stage-indicator',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StageIndicatorComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
+
+  isActive: boolean = false
 
   ngOnInit(): void {
+    console.log('ROUTE-INDICATOR', this.route.url)
   }
 
 }
