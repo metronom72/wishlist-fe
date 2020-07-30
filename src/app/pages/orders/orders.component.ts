c4b83872806c8235ba635ea0b1f990fa454b3874
George <collectionpiter@gmail.com>
2020-07-30 11:06:38 +0300

    adding order fields

--- src/app/pages/orders/orders.component.ts
+++ src/app/pages/orders/orders.component.ts
@@ -9,8 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class OrdersComponent implements OnInit {
   constructor(public orderService: OrderService) {}
 
+  mastercardStatus: boolean = false;
+  adress = null;
+
+  fakeAdress = {
+    apartment: 'Lenina, 29',
+    city: 'Saint-Petersburg',
+    country: 'Russian Federation',
+  };
+
   public currentOrder = {};
   ngOnInit(): void {
+    this.adress = this.fakeAdress;
     this.currentOrder = this.orderService.currentOrder;
     console.log('ORDER_COMPONENT', this.currentOrder);
   }
