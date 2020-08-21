391b5a52ec81e663f98a444ed0336dc6b2994fe4
Dorokhovich Mikhail <mikhail@dorokhovich.ru>
2020-08-22 00:34:09 +0300

    added mock data

--- src/app/components/paginator/paginator.component.ts
+++ src/app/components/paginator/paginator.component.ts
@@ -8,5 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class PaginatorComponent implements OnInit {
   constructor() {}
 
+  activePage = 98;
+
+  perPage = 21;
+
+  total = 100;
+
+  first = 1;
+
+  leftDots = () => this.activePage > 2;
+
+  rightDots = () => this.activePage <= this.total - 2;
+
+  canLeft = true;
+
+  canRight = true;
+
   ngOnInit(): void {}
 }
