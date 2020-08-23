ae8cc9a8cdacf7b3891a3648efe8463f2e38ade4
Dorokhovich Mikhail <mikhail@dorokhovich.ru>
2020-08-23 21:10:51 +0300

    added real categories

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,6 +1,27 @@
 import { Injectable } from '@angular/core';
 import { ICategory } from '../common/category';
-import categories from './categories.json';
+// import categories from './categories.json';
+
+const categories = [
+  {
+    title: 'Мужчинам',
+    order: 1,
+    id: 1,
+    parentId: null,
+  },
+  {
+    title: 'Женщинам',
+    order: 2,
+    id: 2,
+    parentId: null,
+  },
+  {
+    title: 'Детям',
+    order: 3,
+    id: 3,
+    parentId: null,
+  },
+];
 
 @Injectable({
   providedIn: 'root',
